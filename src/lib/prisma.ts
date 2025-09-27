16cee3c1c063dc1ec9651cf2cacc2849dc428369
ugbeadie <ugbeadie3@gmail.com>
2025-09-27 11:53:31 +0100

    complete switch to postgreql

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,25 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var prismaGlobal: PrismaClient | undefined;
+// }
+// const prisma = globalThis.prismaGlobal ?? new PrismaClient();
+// if (process.env.NODE_ENV !== "production") {
+//   globalThis.prismaGlobal = prisma;
+// }
+
+// export default prisma;
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prismaGlobal: PrismaClient | undefined;
-}
-const prisma = globalThis.prismaGlobal ?? new PrismaClient();
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prismaGlobal = prisma;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
